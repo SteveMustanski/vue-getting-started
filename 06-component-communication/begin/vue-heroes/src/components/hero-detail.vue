091ef3425cf24d0388be025afa5311e3c4a54e0b
SteveMustanski <steve.mustanski@gmail.com>
2019-10-15 16:42:46 -0400

    added passing back of child data to parent to update data

--- 06-component-communication/begin/vue-heroes/src/components/hero-detail.vue
+++ 06-component-communication/begin/vue-heroes/src/components/hero-detail.vue
@@ -8,35 +8,31 @@
         <div class="content">
           <div class="field">
             <label class="label" for="id">id</label>
-            <label class="input" name="id" readonly>
-              {{
-              hero.id
-              }}
-            </label>
+            <label class="input" name="id" readonly>{{ clonedHero.id }}</label>
           </div>
           <div class="field">
             <label class="label" for="firstName">first name</label>
-            <input class="input" name="firstName" v-model="hero.firstName" />
+            <input class="input" name="firstName" v-model="clonedHero.firstName" />
           </div>
           <div class="field">
             <label class="label" for="lastName">last name</label>
-            <input class="input" name="lastName" v-model="hero.lastName" />
+            <input class="input" name="lastName" v-model="clonedHero.lastName" />
           </div>
           <div class="field">
             <label class="label" for="description">description</label>
-            <input class="input" name="description" v-model="hero.description" />
+            <input class="input" name="description" v-model="clonedHero.description" />
           </div>
           <div class="field">
             <label class="label" for="originDate">origin date</label>
-            <input type="date" class="input" id="originDate" v-model="hero.originDate" />
+            <input type="date" class="input" id="originDate" v-model="clonedHero.originDate" />
             <p class="comment">
               My origin story began on
-              {{ hero.originDate | shortDate }}
+              {{ clonedHero.originDate | shortDate }}
             </p>
           </div>
           <div class="field">
             <label class="label" for="capeCounter">cape counter</label>
-            <input class="input" name="capeCounter" type="number" v-model="hero.capeCounter" />
+            <input class="input" name="capeCounter" type="number" v-model="clonedHero.capeCounter" />
           </div>
           <div class="field">
             <label class="label" for="capeMessage">cape message</label>
@@ -75,17 +71,24 @@ export default {
       default: () => {},
     },
   },
+  data() {
+    return {
+      clonedHero: { ...this.hero },
+    };
+  },
   computed: {
     fullName() {
-      return this.hero ? `${this.hero.firstName} ${this.hero.lastName}` : '';
+      return this.hero
+        ? `${this.clonedHero.firstName} ${this.clonedHero.lastName}`
+        : '';
     },
   },
   methods: {
     cancelHero() {
-      // placeholder
+      this.$emit('cancel');
     },
     saveHero() {
-      // placeholder
+      this.$emit('save', this.clonedHero);
     },
     handleTheCapes(newValue) {
       const value = parseInt(newValue, 10);
@@ -106,7 +109,7 @@ export default {
     },
   },
   watch: {
-    'hero.capeCounter': {
+    'clonedHero.capeCounter': {
       immediate: true,
       handler(newValue, oldValue) {
         console.log(
