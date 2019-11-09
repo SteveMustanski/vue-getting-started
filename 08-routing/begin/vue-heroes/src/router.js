645aded6db5abacca77264cbc256c9fca0ffe5ff
SteveMustanski <steve.mustanski@gmail.com>
2019-11-09 09:20:51 -0500

    added lazy loading of components and a page not found

--- 08-routing/begin/vue-heroes/src/router.js
+++ 08-routing/begin/vue-heroes/src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Heroes from './views/heroes.vue';
-import HeroDetail from './views/hero-detail.vue';
+import PageNotFound from './views/page-not-found.vue';
+
 
 Vue.use(Router);
 
@@ -16,12 +16,16 @@ export default new Router({
     {
       path: '/heroes',
       name: 'heroes',
-      component: Heroes,
+      //component: Heroes,
+      component: () =>
+        import(/* webpackChunkName: ""bundle-heroes" */ './views/heroes.vue'),
     },
     {
       path: '/heroes/:id',
       name: 'hero-detail',
-      component: HeroDetail,
+      //component: HeroDetail,
+      component: () =>
+        import(/* webpackChunkName: "bundle-heroes" */ './views/hero-detail.vue'),
       props: route => ({ id: parseInt(route.params.id) }),
     },
     {
@@ -31,7 +35,11 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ './views/about.vue'),
+        import(/* webpackChunkName: "bundle-about" */ './views/about.vue'),
+    },
+    {
+      path: '*',
+      component: PageNotFound,
     },
   ],
 });
