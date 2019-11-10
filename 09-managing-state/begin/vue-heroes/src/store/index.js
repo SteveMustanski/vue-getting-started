9f52387f78673564b13ef995c77b6ca9546e9652
SteveMustanski <steve.mustanski@gmail.com>
2019-11-10 16:49:13 -0500

    added getter for getting a hero by id

--- 09-managing-state/begin/vue-heroes/src/store/index.js
+++ 09-managing-state/begin/vue-heroes/src/store/index.js
@@ -20,7 +20,9 @@ const actions = {
   },
 };
 
-const getters = {};
+const getters = {
+  getHeroById: state => id => state.heroes.find(h => h.id === id),
+};
 
 export default new Vuex.Store({
   state,
